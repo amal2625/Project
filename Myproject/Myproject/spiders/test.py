5d24e731fb43de59eb5a9f6f53f1f0ffc9a8566b
Alan K Sunny <amalmathew262555@gmail.com>
2019-09-08 19:19:56 +0530

    scrapy

--- Myproject/Myproject/spiders/test.py
+++ Myproject/Myproject/spiders/test.py
@@ -10,10 +10,16 @@ class TestSpider(scrapy.Spider):
 
 		product_name=response.xpath("//div[@class='countrySelect']")
 
-		for country_name in product_name:
-			yield {
-				'urls': country_name.xpath("//li/a/@href").extract_first()
-			}
+		contry_url=response.xpath("//li/a[@class='sp_country_71']/@href").extract_first()
+		if contry_url is not None:
+			next_page_link=response.urljoin(contry_url)
+			yield scrapy.Request(url=next_page_link, callback=self.test)
+
+
+		# for country_name in product_name:
+		# 	yield {
+		# 		'urls': country_name.xpath("//li/a[@class='sp_country_71']/@href").extract_first()
+		# 	}
 
 
 
@@ -27,7 +33,25 @@ class TestSpider(scrapy.Spider):
 		# 	next_page_link= response.urljoin(next_page)
 		# 	yield scrapy.Request(url=next_page_link, callback=self.parse)
 
-			
+	def test(self, response):
+		sub_name=response.xpath("//div[@class='SPList']/ul[@class='sp noselect']")
+		# sub_names=sub_name.xpath("//ul[@class='sp noselect']")
+		for test in sub_name:
+			new_link=test.xpath(".//li[@class='sp-id']/p[@class='sp-name']/a/@href").extract_first()
+			next_sub_page=response.urljoin(new_link)
+			yield scrapy.Request(url=next_sub_page, callback=self.final_result)
+
+	def final_result(self, response):
+		image_url=response.xpath("//div[@class='profile-container']")
+		first_section=image_url.xpath(".//h1[@class='sp-title']/span").extract_first();
+
+		yield {
+		'Title':first_section
+		}
+
+
+
+
 
 			
         
