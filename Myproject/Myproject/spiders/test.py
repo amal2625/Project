16ddd65bd25d41849f557e28686b5a522e06a3e2
Alan K Sunny <amalmathew262555@gmail.com>
2019-09-10 10:23:41 +0530

    abcd

--- Myproject/Myproject/spiders/test.py
+++ Myproject/Myproject/spiders/test.py
@@ -16,23 +16,6 @@ class TestSpider(scrapy.Spider):
 			yield scrapy.Request(url=next_page_link, callback=self.test)
 
 
-		# for country_name in product_name:
-		# 	yield {
-		# 		'urls': country_name.xpath("//li/a[@class='sp_country_71']/@href").extract_first()
-		# 	}
-
-
-
-
-		# for jok in product_name:
-		# 	yield {
-		# 		'joke_test': jok.xpath(".//div[@class='joke-text']/p").extract_first()
-		# 	}
-		# next_page=response.xpath("//li[@class='next']/a/@href").extract_first();
-		# if next_page is not None:
-		# 	next_page_link= response.urljoin(next_page)
-		# 	yield scrapy.Request(url=next_page_link, callback=self.parse)
-
 	def test(self, response):
 		sub_name=response.xpath("//div[@class='SPList']/ul[@class='sp noselect']")
 		# sub_names=sub_name.xpath("//ul[@class='sp noselect']")
@@ -51,13 +34,40 @@ class TestSpider(scrapy.Spider):
 
 	def final_result(self, response):
 		final_page=response.xpath("//div[@class='profile-container']")
-		title=final_page.xpath(".//h1[@class='sp-title']/span/text()").extract_first();
-		sub_title=final_page.xpath(".//h4/span[@class='sub']/text()").extract_first();
-		primary_location=final_page.xpath(".//div[@class='profile-details']").extract_first()
+		title=final_page.xpath(".//h1[@class='sp-title']/span/text()").extract_first()
+		sub_title=final_page.xpath(".//h4/span[@class='sub']/text()").extract_first()
+		primary_location_master_div=response.xpath("//div[@class='profile-details']")
+		primary_location_label=primary_location_master_div.xpath("//table/tr[1]/td[1]/text()").extract_first()
+		if primary_location_label == 'Primary location:':
+			primary_location=primary_location_master_div.xpath("//table/tr[1]/td[2]/span[@itemprop='address']/span[@itemprop='addressLocality']/text()").extract_first()
+			main_area=primary_location_master_div.xpath("//table/tr[2]/td[2]/a[@class='mainExp']/text()").extract_first()
+		else:
+			primary_location=''
+			main_area=primary_location_master_div.xpath("//table/tr[1]/td[2]/a[@class='mainExp']/text()").extract_first()
+		about=''
+		website=''	
+		language=''
+		section_details_div=response.xpath("//div[@class='profile-content']/div[@class='section details']/table/tr")
+		for section in section_details_div:
+			label_names=section.xpath(".//td[1]/text()").extract_first()
+			if label_names == 'About:':
+				about=section.xpath(".//td[2]/p").extract()
+				
+			if label_names == 'Website:':
+				website=section.xpath(".//td[2]/a/text()").extract_first()
+
+			if label_names == 'Languages spoken:':
+				language=section.xpath(".//td[2]/text()").extract_first()
+		
 		yield {
+		'Page url':response.url,
 		'Title':title,
 		'SubTitle':sub_title,
-		'primary_location':primary_location,
+		'Primary Location':primary_location,
+		'Main Area of Expertize':main_area,
+		'About':about,
+		'Website':website,
+		'Languge Spoken':language
 		}
 
 
